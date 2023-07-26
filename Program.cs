979ae209860d968689e31ac9902661c9a4e0abc4
Denis-Moiseev <mrtesla666@gmail.com>
2023-07-26 21:04:42 +0800

    finish

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿void CounterNumsAboveZero(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) counter++;
+    }
+    Console.Write(counter);
+}
+
+int[] GetArray(string nums)
+{
+    string[] inputNums = nums.Split(' ');
+    int[] array = new int[inputNums.Length];
+    for (int i = 0; i < inputNums.Length; i++)
+    {
+        int.TryParse(inputNums[i], out array[i]);
+    }
+    return array;
+}
+
+string GetInput(string text)
+{
+    Console.Write(text);
+    return (Console.ReadLine());
+}
+
+string nums = GetInput("Введите числа через пробел: ");
+int[] array = GetArray(nums);
+Console.Write($"{String.Join(", ",GetArray(nums))} -> ");
+CounterNumsAboveZero(array);
